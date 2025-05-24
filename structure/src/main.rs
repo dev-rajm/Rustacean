6c87095e631ab584df8a1984471c3fdf98b758be
rajmanna <108714991+dev-rajm@users.noreply.github.com>
2025-05-24 17:59:44 +0530

    structure/update README

---
+++ structure/src/main.rs
@@ -0,0 +1,17 @@
+// Group user field in a single User structure
+struct User {
+    first_name: String,
+    last_name: String,
+    age: u32
+}
+
+fn main() {
+    let user = User {
+        first_name: String::from("Raj"),
+        last_name: String::from("Manna"),
+        age: 69
+    };
+
+    // Access the data from structure
+    println!("First name is {}", user.first_name);
+}
