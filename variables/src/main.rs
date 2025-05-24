efa7aa0dccfe6a42bd03bb5be829dadc288dc617
rajmanna <108714991+dev-rajm@users.noreply.github.com>
2025-05-24 16:14:55 +0530

    variable/is_even

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,27 +1,13 @@
+// 1. Write a function `is_even` that takes a number as an input and return true if even, false otherwise
+
 fn main() {
+    let ans = is_even(13);
+    println!("{}", ans);
+}
 
-    // 🙅‍♂️ Constants
-    const MY_AGE: u8 = 21; 
-    println!("My age is {MY_AGE}");
-
-    // 👎 Immutable Variables
-    let name = "Raj Manna";
-    println!("My name is {name}");
-    
-    // 👍 Mutable Variables
-    let mut total_marks = 466;
-    println!("My total marks is {total_marks}");
-
-    // 🥷 Shadowing the Variable
-    let sum = 2; // Original Variable
-    let sum = sum + 5; // Create a new sum variable which shadowed the original value(2) of sum and then adding 5 into it.
-
-    // Block Level Shadowing(Only replace the pervious value in the block level)
-    {
-        let sum = sum * 2; // 7*2 = 14
-        println!("The value of x in the inner scope is: {sum}"); // Verify the block level changes
+fn is_even(num: i32) -> bool {
+    if num % 2 == 0 {
+        return true;
     }
-
-    println!("Value of x is {sum}"); // Global changes
-
+    return false;
 }
